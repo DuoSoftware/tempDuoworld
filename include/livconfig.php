ae97c3e80f129c7548f1bae56448b78c888d0b7a
Pamidu jayaranga <pamidu@live.com>
2016-11-21 15:02:04 +0530

    config

--- include/livconfig.php
+++ include/livconfig.php
@@ -1,7 +1,7 @@
 <?php
-$mainDomain="duoworld.com";
-$authURI="http://auth.duoworld.com:3048/";
-$objURI="http://obj.duoworld.com:3000/";
+$mainDomain="mambati.com";
+$authURI="http://auth.mambati.com:3048/";
+$objURI="http://obj.mambati.com:3000/";
 $fullhost=strtolower($_SERVER['HTTP_HOST']);
 $protacall="https";
 define("ROOT_PATH", $_SERVER['DOCUMENT_ROOT']);
@@ -10,14 +10,10 @@ define("APPICON_PATH", "/var/www/html/devportal/appicons");
         //define("BASE_PATH", "/var/www/html/medialib");
         //define("STORAGE_PATH", BASE_PATH . "/media");
         //define("THUMB_PATH", BASE_PATH . "/thumbnails");
-define("SVC_OS_URL", "http://obj.duoworld.com:3000");
-define("SVC_OS_BULK_URL", "http://obj.duoworld.com:3001/transfer");
-define("SVC_AUTH_URL", "http://auth.duoworld.com:3048");
-define("SVC_CEB_URL", "http://admin.dev.duoworld.com:3500");
-define("SVC_SMOOTHFLOW_URL", "http://smoothflow.io");
-define ("STRIPE_KEY","sk_test_ujsydm6IgtDJ49L74x7sj4x7");
-
-define ("PAYMENT_GATWAY", "stripe");
+define("SVC_OS_URL", "http://obj.mambati.com:3000");
+define("SVC_OS_BULK_URL", "http://obj.mambati.com:3001/transfer");
+define("SVC_AUTH_URL", "http://auth.mambati.com:3048");
+define("SVC_CEB_URL", "http://admin.dev.mambati.com:3500");
 
 define("STORAGE_PROFILE", "PROXY");
 define("SVC_MEDIA_URL", "dw-storage");
